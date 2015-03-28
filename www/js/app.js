b571f176e987afadcaae5a27fc64cb70d594af22
mariekeely <marie_keely@yahoo.co.uk>
2015-03-28 00:01:02 +0000

    counting

--- www/js/app.js
+++ www/js/app.js
@@ -1,38 +1,48 @@
 angular.module('calorific', ['ionic'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
-
+  
+    // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/');
-
+    
     $stateProvider
+    
+    // this is the food selection type
     .state('state1', {
         url: '/',
         templateUrl: 'templates/state1.html'
     })
 
+    // this is the food and weights
     .state('state2', {
         url: '/state2',
         templateUrl: 'templates/state2.html'
     })
 
+    // this is the weight counter
     .state('state3', {
         url: '/state3',
         templateUrl: 'templates/state3.html'
     });
+    
+    /*
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'    
+    })
+    
+    .state('tab.about', {
+        url: '/about',
+        templateUrl: 'templates/about.html'
+    })
+    
+    .state('tab.contact', {
+        url: '/contact',
+        templateUrl: 'templates/contact.html'
+    })
+    */
 
 })
 
-.run(function($ionicPlatform) {
-  
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-  
-})

7e43231150bf3ce23264d7f28019489e594746a7
mariekeely <marie_keely@yahoo.co.uk>
2015-04-08 21:53:56 +0100

    controllers

--- www/js/app.js
+++ www/js/app.js
@@ -1,48 +1,67 @@
-angular.module('calorific', ['ionic'])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
-  
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/');
-    
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
     $stateProvider
-    
-    // this is the food selection type
-    .state('state1', {
-        url: '/',
-        templateUrl: 'templates/state1.html'
-    })
 
-    // this is the food and weights
-    .state('state2', {
-        url: '/state2',
-        templateUrl: 'templates/state2.html'
-    })
-
-    // this is the weight counter
-    .state('state3', {
-        url: '/state3',
-        templateUrl: 'templates/state3.html'
-    });
-    
-    /*
     // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'    
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
     })
-    
+
+    // the pet tab has its own child nav-view and history
+    .state('tab.pet-index', {
+        url: '/pets',
+        views: {
+            'pets-tab': {
+                templateUrl: 'templates/pet-index.html',
+                controller: 'PetIndexCtrl'
+            }
+        }
+    })
+
+    .state('tab.pet-detail', {
+        url: '/pet/:petId',
+        views: {
+            'pets-tab': {
+                templateUrl: 'templates/pet-detail.html',
+                controller: 'PetDetailCtrl'
+            }
+        }
+    })
+
     .state('tab.about', {
         url: '/about',
-        templateUrl: 'templates/about.html'
+        views: {
+            'about-tab': {
+                templateUrl: 'templates/about.html'
+            }
+        }
     })
-    
+
     .state('tab.contact', {
         url: '/contact',
-        templateUrl: 'templates/contact.html'
-    })
-    */
+        views: {
+            'contact-tab': {
+                templateUrl: 'templates/contact.html'
+            }
+        }
+    });
 
-})
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/pets');
 
+});
---
+++ www/js/services.js
@@ -0,0 +1,42 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('PetService', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var pets = [
+        {
+            id: 0,
+            title: 'Breakfast',
+            description: 'Cereal, Bread, pastries, milk, egg, sausage, puddin, rasher, potatoe chips'
+        },
+        {
+            id: 1,
+            title: 'Lunch',
+            description: 'Wrap, Sandwich, Soup, Lasagne, Fish pie, Chicken, Beef, Turkey'
+        },
+        {
+            id: 2,
+            title: 'Dinner',
+            description: 'Lamb, Burger, Sheperds pie, Lasagne, Fish pie, Chicken, Beef, Turkey, Pasta Bake, Vegetables'
+        },
+        {
+            id: 3,
+            title: 'Snacks',
+            description: 'Biscuits, Cheese, Crisps, Pizza, Sweets, Yoghurt, Chocolate'
+        }
+  ];
+
+    return {
+        all: function () {
+            return pets;
+        },
+        get: function (petId) {
+            // Simple index lookup
+            return pets[petId];
+        }
+    }
+});

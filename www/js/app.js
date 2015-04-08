804018153fc4d6328cd280ed0a61d12a4d0ef65f
mariekeely <marie_keely@yahoo.co.uk>
2015-04-08 22:11:49 +0100

    food list

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,15 @@
-// Ionic Starter App
+// Ionic calorific App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'calorific' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+// 'calorific.services' is found in services.js
+// 'calorific.controllers' is found in controllers.js
+angular.module('calorific', ['ionic', 'calorific.services', 'calorific.controllers'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
+    // using AngularUI Router which uses the concept of states
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider
